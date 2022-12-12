4de7fc183bb5108d8509f4d209ddf69bfb274a2d
Сергей Абросов <abrosovsergeya@gmail.com>
2022-12-13 00:20:02 +0300

    New Diagram and deleting button

--- JS/Web-application.js
+++ JS/Web-application.js
@@ -33,7 +33,7 @@
 
 let wrapper = document.querySelector('.img__wrapper');
 
-function donwload(input) {
+function downloadPhoto(input) {
     let file = input.files[0];
     let reader = new FileReader();
     reader.readAsDataURL(file);
@@ -45,3 +45,21 @@ function donwload(input) {
         img.src = reader.result;
     }
 }
+
+function deletePhoto() {
+    let img = document.createElement('img');
+    // <img width="800" height="600" src="../Images/UploadPhoto.jpg" alt="Фото указывающее на место для разметки" usemap="#useCase">
+    img.width = 800;
+    img.height = 600;
+    img.src = "../Images/UploadPhoto.jpg";
+    img.alt = "Фото указывающее на место для разметки";
+    img.useMap = "#useCase";
+
+    wrapper.removeChild(wrapper.children[0]);
+    wrapper.appendChild(img);
+}
+
+// + include / пунктик / обратная стрелка / 3 / не кнопка для увеличения громкости, а увеличение громкости 
+
+// canvas - холст ()
+// реализовать по 2-3 пункта в сценариях использования и отправить (для каждого)
